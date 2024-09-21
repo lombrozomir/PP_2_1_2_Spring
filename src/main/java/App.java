dd75b5271ed15bc1a61ed41fb4f6785f609d00af
lombrozomir <roman.ryzhkov31@gmail.com>
2024-09-21 17:10:18 +0300

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,6 @@ public class App {
         HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         Cat beanCat = (Cat) applicationContext.getBean("cat");
 
-
         System.out.println(bean.equals(bean));
         System.out.println(bean.equals(beanCat));
         System.out.println(beanCat.equals(beanCat));
