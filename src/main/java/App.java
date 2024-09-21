e14a2701a169b103cec2f773ee0e84fba28dade1
lombrozomir <roman.ryzhkov31@gmail.com>
2024-09-21 17:01:35 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,16 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println(bean.equals(bean));
+        System.out.println(bean.equals(beanCat));
+        System.out.println(beanCat.equals(beanCat));
+
+    }
+}
